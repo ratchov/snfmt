305c20b36204f000d014976b9cc424d264e508c9
Alexandre Ratchov <alex@caoua.org>
2024-05-27 14:52:20 +0200

    snfmt: No need to include errno.h

--- snfmt.c
+++ snfmt.c
@@ -13,7 +13,6 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-#include <errno.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stddef.h>
