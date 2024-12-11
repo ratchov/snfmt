fbf9112fa058aae747f79b7f86e458a25326c743
Alexandre Ratchov <alex@caoua.org>
2024-12-11 20:09:11 +0100

    Use ', ## __VA_ARGS__' to handle empty args list

--- snfmt.h
+++ snfmt.h
@@ -47,6 +47,6 @@ size_t snfmt_va(snfmt_func *, char *, size_t, const char *, va_list);
  * unreachable call to snprintf() on which MSVC performs the checks.
  */
 #define snfmt(cb, p, n, ...) \
-	(0 ? (size_t)snprintf((p), (n), __VA_ARGS__) : snfmt((cb), (p), (n), __VA_ARGS__))
+	(0 ? (size_t)snprintf((p), (n), ## __VA_ARGS__) : snfmt((cb), (p), (n), ## __VA_ARGS__))
 
 #endif
